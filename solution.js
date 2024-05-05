c9dcd07c4a84f9e2135d03c1415fe65355df395a
Shivam Kumar Singh <90489199+shivamsingh-git-codes@users.noreply.github.com>
2024-05-05 18:46:16 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 app.use(express.static("public"));
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs"); 
 });
 
 app.get("/about", (req, res) => {
